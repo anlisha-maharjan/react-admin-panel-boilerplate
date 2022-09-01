d80fedd3d34a39f7b5ff23019564c7e4638986a1
anlishamaharjan <anlishamaharjan@gmail.com>
2022-09-01 15:51:01 +0545

    feat: import redux and material ui provider in root

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { SplashScreenProvider } from "src/configs/core";
+import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+import "./assets/scss/main.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <SplashScreenProvider>
+      <App />
+    </SplashScreenProvider>
   </React.StrictMode>
 );
 
