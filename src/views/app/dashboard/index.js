66c51701df36d0d56f953d4a4e7c4ee10e99983b
anlishamaharjan <anlishamaharjan@gmail.com>
2022-09-01 15:59:29 +0545

    feat: add dashboard page

---
+++ src/views/app/dashboard/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import * as Mui from "@mui/material";
+
+export default function Dashboard() {
+  return (
+    <Mui.Typography component="h5" variant="h5" className="mr-auto font-weight-medium">
+      Welcome!
+    </Mui.Typography>
+  );
+}
