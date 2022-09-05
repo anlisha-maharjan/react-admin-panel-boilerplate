f472392b9b0b39a8b09064341622dd4e17db97c8
anlishamaharjan <anlishamaharjan@gmail.com>
2022-09-05 18:06:49 +0545

    feat: add error boundary page

--- src/App.js
+++ src/App.js
@@ -7,19 +7,22 @@ import { Provider } from "react-redux";
 import store from "src/reduxs/store";
 import { LayoutSplashScreen } from "src/configs/core";
 import theme from "src/configs/theme";
+import ErrorBoundary from "src/components/error-boundary";
 import Root from "./Root";
 
 export default function App() {
   return (
     /* Provide Redux store */
     <Provider store={store}>
-      {/* Add high level `Suspense` in case if was not handled inside the React tree. */}
-      <Suspense fallback={<LayoutSplashScreen />}>
-        <ThemeProvider theme={theme}>
-          {/* Render routes with provided `Layout`. */}
-          <Root />
-        </ThemeProvider>
-      </Suspense>
+      <ErrorBoundary>
+        {/* Add high level `Suspense` in case if was not handled inside the React tree. */}
+        <Suspense fallback={<LayoutSplashScreen />}>
+          <ThemeProvider theme={theme}>
+            {/* Render routes with provided `Layout`. */}
+            <Root />
+          </ThemeProvider>
+        </Suspense>
+      </ErrorBoundary>
     </Provider>
   );
 }
