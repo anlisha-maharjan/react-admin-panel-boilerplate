9d10c88198181e9eedcad0e742bcfd157bdbebbf
anlisha-maharjan <anlishamaharjan@gmail.com>
2022-12-06 11:59:37 +0545

    fix: remove unused form component

--- src/components/form/input-hidden-field.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useField } from "formik";
-
-const InputHiddenField = ({ ...props }) => {
-  const [field] = useField(props);
-  return <input type="hidden" {...field} {...props} />;
-};
-
-export default InputHiddenField;
